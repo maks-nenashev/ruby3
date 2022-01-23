0c2327566d7542d3d301701a5dca9deeef14a47a
maksym-nenashev <nenashev@yahoo.com>
2022-01-23 16:29:43 +0100

    arr

---
+++ app1.rb
@@ -0,0 +1,18 @@
+arr[red, blac, blue, dark]
+
+while 2 + 2 == 4
+    
+puts"Color"
+color = gets.strip
+ 
+
+if color == "stop"
+    puts arr
+    exit
+ end
+    arr << color
+end
+
+puts "Program finished. Press Enter to exit."
+gets
+ 
