ea50d6a8ffaaadada7fa0144df2b71545df5a5b1
maksym-nenashev <nenashev@yahoo.com>
2022-02-08 15:51:41 +0100

    enter arr

--- app7.rb
+++ app7.rb
@@ -1,24 +1,22 @@
 arr = []
 loop do
-  puts "Enter name?"
+  puts "Enter name to arr?"
     s = gets.strip
-if s == ""
-     break #!!!
-    arr << s
-end
-    
+    if s == ""
+      break #!!!
+ end
+     arr << s
+
     x = 0
 
 arr.each do |m|
   
-    puts"#{x} #{m}"
+    puts"#{x}. #{m}"
       
   x = x + 1
 
+ end
 end
-
-end
-
 
 puts "Program finished. Press Enter to exit."
 gets
