70ff8f98108e3e71e2b3137f4d9d4a41b6f71e0b
maksym-nenashev <nenashev@yahoo.com>
2022-02-17 19:11:38 +0100

    RSP

---
+++ app11.rb
@@ -0,0 +1,55 @@
+puts "(R)ock, (S)cissoris, (P)aper"
+s = gets.strip.capitalize
+
+   if s == "R"
+    user_choice = :rock
+elsif s == "S"
+    user_choice = :scissoris
+elsif s == "P"
+    user_choice = :paper
+else
+        puts "Can`t understand whte you want, sorry..."
+ end
+
+ arr = [:rock, :scissoris, :paper]
+    
+computer_choice = arr[rand(0..2)] #computer choise
+
+puts"User choice: #{user_choice}"
+puts "Computer choise: #{computer_choice}"
+
+matrix = [
+ 
+#for draw
+[:rock, :rock, :draw],
+[:scissoris, :scissoris, :draw],
+[:paper, :paper, :draw],
+ 
+#for rock
+[:rock, :scissoris, :first_win],
+[:rock, :papir, :second_win],
+ 
+#for scissoris
+[:scissoris, :rock, :second_win],
+[:scissoris, :papir, :fiers_win],
+ 
+#for papir
+[:papir, :rock, :first_win],
+[:papir, :scissoris, :second_win],
+]
+
+matrix.each do |item|  #method inspect
+ 
+    if item[0] == user_choice && item[1] == computer_choice
+end
+
+   if item[2] == :first_win
+    puts "User wins!"
+elsif item[2] == :second_win
+    puts "Computer wins!"
+elsif item[2] == :draw
+    puts "Draw!"
+end
+end
+puts "Program finished. Press Enter to exit."
+gets
