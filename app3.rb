5257f29e432a9666eb5f2e8bbb0a4a3b23e79e1b
maksym-nenashev <nenashev@yahoo.com>
2022-02-19 20:13:33 +0100

    %w

--- app3.rb
+++ app3.rb
@@ -1,15 +1,15 @@
 num = 4
 
 arr = %w[1 22 33 6.1 max]
-puts(arr)
+puts arr
 
 
-names = Array['Bob',"Max","Zak"] 
-puts(names[0])
+names = ['Bob',"Max","Zak"] 
+puts names[0]
 
-nam = Array['Bob',"Max","Zak"] 
+nam = ['Bob',"Max","Zak"] 
 nam[0] = 'Diana'
-puts(nam)
+puts nam
 
 puts "Program finished. Press Enter to exit."
 gets
