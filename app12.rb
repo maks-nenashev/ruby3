7b754879225acb012a154c315e2abf6a4384eac2
maksym-nenashev <nenashev@yahoo.com>
2022-02-19 17:46:28 +0100

    Hash

---
+++ app12.rb
@@ -0,0 +1,23 @@
+
+  phonebook = {}
+  
+loop do
+
+print "Enter your name!"
+    name = gets.strip
+      
+      if name == ""
+          break # !!!!
+      end
+
+print"Enter your phone!"
+    phone_number = gets.strip
+  
+    phonebook[name] = phone_number
+
+      end
+puts phonebook
+    
+puts "Program finished. Press Enter to exit."
+gets
+ 
