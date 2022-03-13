08fd800aaeb7fe23e6b7259b3995ab72e8f732bb
maksym-nenashev <nenashev@yahoo.com>
2022-03-13 23:02:51 +0100

    Proverka klucza v slovare

---
+++ app20.rb
@@ -0,0 +1,11 @@
+hh = {"cat" => ["кот", "кошка"],"dog" => ["собака","пес"], "girl" => ["девочка", "телка"]}
+  
+if hh.has_key? "cat"
+
+puts"V slovare est slovo cat!"
+
+end
+
+puts "Program finished. Press Enter to exit."
+gets
+ 
