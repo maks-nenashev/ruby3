c30edcd5505dc52bc0b5665de92594ff400a8829
maksym-nenashev <nenashev@yahoo.com>
2022-04-14 23:10:04 +0200

    name book2

---
+++ app26.rb
@@ -0,0 +1,34 @@
+ @hh = {}
+ 
+     def add_person options       #dobowlenie pary w hash
+         puts"Already exists" if @hh[options[:name]] #<<<<<<Wazno!
+         @hh[options[:name]] = options[:age]  
+      end
+  
+     def show_hash                   #pokazywaet hash    
+       @hh.keys.each do |key|
+              age = @hh[key]
+              puts"Name: #{key}, Age: #{age}"
+       end
+      end
+   
+loop do
+                                     #Dobowlaet znaczenie 
+    print "Enter name!"
+    name = gets.strip
+     
+    if name == ""
+          show_hash
+          break
+      end
+  
+    print"Enter age!"
+    age = gets.to_i
+      
+    options = {:name => name, :age => age}
+    add_person options
+  end
+
+puts "Program finished. Press Enter to exit."
+gets
+ 
