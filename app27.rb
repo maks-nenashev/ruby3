ae0b5f601c1bccbbbfe90ab00a84f006d3be4d39
maksym-nenashev <nenashev@yahoo.com>
2022-04-15 10:35:00 +0200

    "merge"

---
+++ app27.rb
@@ -0,0 +1,18 @@
+def show_book book                      
+       book.keys.each do |key|
+              age = book[key]
+              puts"Name: #{key}, Age: #{age}"
+       end
+      end
+   
+book1 = {"Anna" => 12, "Gus" => 23, "Lida" => 43}  
+
+book2 = {"Walt" => 33, "Lussi" => 22}
+
+book = book1.merge book2 #Objedinenie hashew w odin "merge"
+
+show_book book
+
+puts "Program finished. Press Enter to exit."
+gets
+ 
